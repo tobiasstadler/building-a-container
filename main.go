8b5c58847bc1b90dcf3321bc8fa55e56e993905d
Tobias Stadler <ts.stadler@gmx.de>
2019-11-04 19:54:13 +0100

    Set random hostname before process start

--- main.go
+++ main.go
@@ -2,25 +2,53 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"os/exec"
+	"strconv"
 	"syscall"
+	"time"
 )
 
 func main() {
-	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+	if os.Args[1] == "run" {
+		cmd := exec.Command(os.Args[0], append([]string{"doRun"}, os.Args[2:]...)...)
 
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
 
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		cmd.SysProcAttr = &syscall.SysProcAttr{
+			Cloneflags: syscall.CLONE_NEWUTS,
+		}
+
+		err := cmd.Run()
+		if err != nil {
+			fmt.Printf("Error running the %s command: %s\n", os.Args[0], err)
+			os.Exit(1)
+		}
+	} else if os.Args[1] == "doRun" {
+		initCmd()
+
+		err := syscall.Exec(os.Args[2], os.Args[3:], os.Environ())
+		if err != nil {
+			fmt.Printf("Error running the %s command: %s\n", os.Args[2], err)
+			os.Exit(1)
+		}
+	} else {
+		fmt.Printf("Unknown command: %s\n", os.Args[1])
+		os.Exit(1)
 	}
+}
 
-	err := cmd.Run()
+func initCmd() {
+	rand.Seed(time.Now().UnixNano())
+
+	containerName := "container" + strconv.Itoa(rand.Int())
+
+	err := syscall.Sethostname([]byte(containerName))
 	if err != nil {
-		fmt.Printf("Error running the %s command: %s\n", os.Args[1], err)
+		fmt.Printf("Error setting hostname to %s: %s\n", containerName, err)
 		os.Exit(1)
 	}
 }
